75349ee421253c733a295fe47252eb37532493a1
Somayyeh094 <s.somayyeh.seyedi@gmail.com>
2025-07-01 19:14:51 +0330

    added form

--- src/App.js
+++ src/App.js
@@ -1,27 +1,51 @@
-import { useState,useEffect } from 'react';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 import sunIcon from "./sunIcon.svg";
 import moonIcon from "./moonIcon.svg";
+import magnifier from "./magnifier.svg";
+import axios from "axios";
 function App() {
-
+  let [word, setWord] = useState("");
   function changeTheme() {
     let body = document.querySelector(".App");
     body.classList.toggle("dark");
     document.querySelector(".moon-color").classList.toggle("hidden");
     document.querySelector(".sun-color").classList.toggle("hidden");
   }
+  function getResponse(response) {}
+  function getApi(event) {
+    event.preventDefault();
+    axios
+      .get(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+      .then(getResponse);
+  }
+  function getWord(event) {
+    setWord(event.target.value);
+  }
   return (
     <div className="App">
-      <button class="change-theme" onClick={changeTheme}>
-            <img
-              src={sunIcon}
-              width="25"
-              alt="light-mode"
-              className="sun-color hidden" title='Light theme'
-            />{" "}
-            <img src={moonIcon} width="25" alt="light-mode" className="moon-color" title='Dark theme' />
-          </button>
-      <h1>Hello</h1>
+      <header className="">
+        <button class="change-theme" onClick={changeTheme}>
+          <img
+            src={sunIcon}
+            alt="light-mode"
+            className="sun-color hidden"
+            title="Light theme"
+          />{" "}
+          <img
+            src={moonIcon}
+            alt="light-mode"
+            className="moon-color"
+            title="Dark theme"
+          />
+        </button>
+      </header>
+      <form onSubmit={getApi} className="text-center">
+        <input type="search" placeholder="Search a word" onChange={getWord} />
+        <button>
+          <img src={magnifier} width="25" alt="search icon" />
+        </button>
+      </form>
     </div>
   );
 }
