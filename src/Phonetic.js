3fcb49a65041a467d1ae8c366ccc6143cd415c27
Somayyeh094 <s.somayyeh.seyedi@gmail.com>
2025-07-03 15:35:11 +0330

    capitalized first letter(phonetics.js)

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -2,19 +2,25 @@ import "./Phonetic.css";
 import audioIcon from "./audioIcon.svg";
 export default function Phonetic({ data }) {
   if (data != null) {
-      let phonetics = data[0].phonetics;
-      function playAudio() {
-        document.getElementById("myAudio").play();
-      }
-
+    let phonetics = data[0].phonetics;
+    function playAudio() {
+      document.getElementById("myAudio").play();
+    }
 
     return (
       <div className="Phonetic">
+        <h1>{data[0].word.charAt(0).toUpperCase() + data[0].word.slice(1)}</h1>
         {phonetics.map(function (phonetic, index) {
           return (
             <span key={index}>
               {" "}
-              <img src={audioIcon} width={"30px"} onClick={playAudio} className="pointer" alt="audio" />
+              <img
+                src={audioIcon}
+                width={"30px"}
+                onClick={playAudio}
+                className="pointer"
+                alt="audio"
+              />
               <audio src={phonetic.audio} id="myAudio" controls></audio>{" "}
               {phonetic.text}
             </span>
