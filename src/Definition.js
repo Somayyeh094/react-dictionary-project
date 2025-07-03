a6ce2c15ab3a925c40c2e9387c906f6a1955db83
Somayyeh094 <s.somayyeh.seyedi@gmail.com>
2025-07-03 15:22:39 +0330

    added copy-btn to definition.js

--- src/Definition.js
+++ src/Definition.js
@@ -1,11 +1,14 @@
-import "./Definition.css"
-
-
+import "./Definition.css";
 
 export default function Definition({ data }) {
   if (data != null) {
-    let definitions = data;
-
+      let definitions = data;
+      function copyText() {
+          let section= document.getElementById("section")
+      navigator.clipboard.writeText(section.innerText);
+      alert("Quotes copied!");
+    
+}
     return (
       <div className="Definition">
         {definitions.map(function (define, index) {
@@ -13,10 +16,12 @@ export default function Definition({ data }) {
             <div key={index}>
               {define.meanings.map(function (defineword, index) {
                 return (
-                  <div className="word-section" key={index}>
-                    <h2>
-                    {defineword.partOfSpeech}
-                    </h2>
+                  <div className="word-section" id="section" key={index}>
+                    <button class="copy-btn" id="copy-btn" onClick={copyText}>
+                      Copy
+                        </button>
+                        <br/>
+                    <h2>{defineword.partOfSpeech}</h2>
                     <div>
                       {defineword.definitions.map(function (word, index) {
                         return (
@@ -25,6 +30,7 @@ export default function Definition({ data }) {
                               <strong> {index + 1}-Definition</strong>:{" "}
                               {word.definition}
                             </p>
+
                             <p>
                               <strong>Example</strong>: <em>{word.example}</em>
                             </p>
