36497adc3c1803e35783aefa4e38ac7702be0a6b
Somayyeh094 <s.somayyeh.seyedi@gmail.com>
2025-07-04 17:32:51 +0330

    updated axios to catch errors

--- src/Search.js
+++ src/Search.js
@@ -33,10 +33,35 @@ export default function Search() {
     console.log(photos);
   }
 
+  function errors(error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+      alert("Please type the word correctly!");
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+      // http.ClientRequest in node.js
+      alert("Please try it later...")
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log("Error", error.message);
+      alert("Please try again!")
+    }
+    console.log(error.config);
+  
+}
+
+
   function getApi() {
     axios
       .get(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
-      .then(getResponse);
+      .then(getResponse).catch((errors))
+      
   }
   function handleForm(event) {
     event.preventDefault();
